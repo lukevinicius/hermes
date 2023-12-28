23277057ceea9e9cb443ba9a323fd36009a40463
lukevinicius <lucasviniciusaalves@gmail.com>
2023-12-28 11:19:51 -0300

    feat: send unique email

--- src/http/routes/emails/index.ts
+++ src/http/routes/emails/index.ts
@@ -1,5 +1,6 @@
 import { Elysia } from 'elysia'
 
 import { massEmailSending } from './mass-email-sending.controller'
+import { sendEmail } from './send-email.controller'
 
-export const emailRoutes = new Elysia().use(massEmailSending)
+export const emailRoutes = new Elysia().use(massEmailSending).use(sendEmail)
